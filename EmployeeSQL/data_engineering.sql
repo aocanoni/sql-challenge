f5fa2886d1180d1d30402ae95b5857578f326779
aocanoni <157078630+aocanoni@users.noreply.github.com>
2024-06-02 17:12:03 -0700

    added data_modeling png

--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -1,32 +1,32 @@
 DROP TABLE employees;
 
 CREATE TABLE departments (
-    dept_no VARCHAR NOT NULL,
-    dept_name VARCHAR NOT NULL,
+    dept_no VARCHAR(30) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp (
     emp_no INTEGER NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
 	PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
 	emp_no INTEGER NOT NULL,
 	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE employees (
     emp_no INTEGER NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
-    birth_date VARCHAR NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
-    hire_date VARCHAR NOT NULL,
+    emp_title_id VARCHAR(30) NOT NULL,
+    birth_date VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(30) NOT NULL,
+    hire_date VARCHAR(30) NOT NULL,
 	PRIMARY KEY (emp_no)
 );
 
@@ -38,7 +38,7 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
-	title_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
+	title_id VARCHAR(30) NOT NULL,
+	title VARCHAR(30) NOT NULL,
 	PRIMARY KEY (title_id)
 );
